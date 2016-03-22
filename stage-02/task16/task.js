aa687da06d3862f2eebac9471c98cafdbe4d1aa3
Sun Lifeng <1104506375@qq.com>
2016-03-22 15:38:27 +0800

    task 16

--- stage-02/task16/task.js
+++ stage-02/task16/task.js
@@ -9,57 +9,101 @@
  *    "上海": 40
  * };
  */
-var aqiData = new Object();
-;
+var aqiData = {};
+var cityValue = document.getElementById('aqi-city-input');
+var qualityValue = document.getElementById('aqi-value-input');
 
 /**
  * 从用户输入中获取数据，向aqiData中增加一条数据
  * 然后渲染aqi-list列表，增加新增的数据
  */
 function addAqiData() {
-    var cityValue = document.getElementById('aqi-city-input').value;
-    var qualityValue = document.getElementById('aqi-value-input').value;
-    aqiData.city = "北京";
-    aqiData.quality = 60;
-    return aqiData;
+    var cityValueTrim = cityValue.value.trim();
+    var qualityValueTrim = qualityValue.value.trim();
+
+    // 验证城市名称
+    function checkCity() {
+        if (cityValueTrim !== "" && cityValueTrim !== null) {
+            if (!cityValueTrim.match(/^[A-Za-z\u4E00-\u9FA5]+$/)) {
+                alert("城市名称格式错误，请重新输入!");
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            alert("城市名称不能为空哦！");
+            return false;
+        }
+    }
+
+    // 验证空气质量指数
+    function checkQuality() {
+        if (qualityValueTrim !== "" && qualityValueTrim !== null) {
+            if (!qualityValueTrim.match(/^-?[1-9]\d*$/)) {
+                alert("空气质量指数格式错误，请重新输入!");
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            alert("空气质量指数不能为空哦！");
+            return false;
+        }
+    }
+
+    var checkCityTag = checkCity();
+    var checkQualityTag = checkQuality();
+
+    // 返回aqiData
+    if (checkCityTag == true && checkQualityTag == true) {
+        aqiData[cityValueTrim] = qualityValueTrim;
+    }
 }
-
- alert(addAqiData());
-
 
 /**
  * 渲染aqi-table表格
  */
 function renderAqiList() {
-
+    var aqiTable = document.getElementById('aqi-table');
+    var _html = '<tr> <td>城市</td> <td>空气质量</td> <td>操作</td> </tr>';
+    for (var city in aqiData) {
+        _html += '<tr><td>' + city + '</td><td>' + aqiData[city] + '</td><td><button data-city="' + city + '">删除<button></button></td>';
+    }
+    // 三目运算符
+    aqiTable.innerHTML = city ? _html : '';
 }
 
-/**
- * 点击add-btn时的处理逻辑
- * 获取用户输入，更新数据，并进行页面呈现的更新
- */
+// 添加操作
 function addBtnHandle() {
-    alert(addAqiData());
+    addAqiData();
     renderAqiList();
 }
 
-/**
- * 点击各个删除按钮的时候的处理逻辑
- * 获取哪个城市数据被删，删除数据，更新表格显示
- */
+// 删除操作
 function delBtnHandle() {
-    // do sth.
-
+    var city=this.parentNode.parentNode.firstChild.innerHTML;
+    delete aqiData[city];
     renderAqiList();
 }
 
+// 初始化
 function init() {
-    // 点击时触发addBtnHandle函数
-   // var addBtn = document.getElementById('add-btn');
-   // addBtn.onclick = addBtnHandle();
+    var addBtn = document.getElementById('add-btn');
+    var tableBtn = document.getElementById('aqi-table');
 
-    // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
+    addBtn.onclick = function () {
+        addBtnHandle();
+    }
 
+    // 事件委托
+    tableBtn.onclick = function (ev) {
+        var ev = ev || window.event;
+        var target=ev.target||ev.srcElement;
+        if(target.nodeName.toLowerCase()=='button'){
+            // call 或 apply
+            delBtnHandle.call(target);
+        }
+    }
 }
 
 init();
