8e37ff573afec84208cc5e71258863ba90eed08b
Sun Lifeng <1104506375@qq.com>
2016-03-23 11:35:24 +0800

    task 17

--- stage-02/task17/task.js
+++ stage-02/task17/task.js
@@ -56,57 +56,92 @@ var pageState = {
     nowGraTime: "day"
 }
 
-// 渲染select
-function renderSelect() {
+// 渲染图表
+function renderChart() {
+    var charWrap = document.getElementById('aqi-chart-wrap');
+    var nowCity = pageState.nowSelectCity;
+    var _html = '<h2>' + nowCity + '空气质量监测数据</h2>';
+    charWrap.className = pageState.nowGraTime;
+    for (var dat in aqiSourceData[nowCity]) {
+        var quality = aqiSourceData[nowCity][dat];
+        if (quality >= 400) {
+            _html += '<div class="column"><div class="colSender" title="' + dat + ':' + quality;
+            _html += '" style="height:' + quality + 'px;background: #000;"></div></div>';
+        } else if (quality >= 300) {
+            _html += '<div class="column"><div class="colSender" title="' + dat + ':' + quality;
+            _html += '" style="height:' + quality + 'px;background: purple;"></div></div>';
+        } else if (quality >= 200) {
+            _html += '<div class="column"><div class="colSender" title="' + dat + ':' + quality;
+            _html += '" style="height:' + quality + 'px;background: red"></div></div>';
+        } else if (quality >= 100) {
+            _html += '<div class="column"><div class="colSender" title="' + dat + ':' + quality;
+            _html += '" style="height:' + quality + 'px;background: blue;"></div></div>';
+        } else {
+            _html += '<div class="column"><div class="colSender" title="' + dat + ':' + quality;
+            _html += '" style="height:' + quality + 'px;background: green;"></div></div>';
+        }
+    }
+    charWrap.innerHTML = _html;
+}
+
+// 日、周、月的radio事件点击时的处理函数
+function graTimeChange() {
+    // 确定是否选项发生了变化
+    var timeList = document.getElementById('form-gra-time').getElementsByTagName('input');
+    for (var i = 0; i < timeList.length; i++) {
+        if (timeList[i].checked) {
+            var timeValue = timeList[i].value;
+        }
+    }
+    // 设置对应数据
+    pageState.nowGraTime = timeValue;
+
+    // 调用图表渲染函数
+    renderChart();
+}
+
+// select发生变化时的处理函数
+function citySelectChange() {
+    // 确定是否选项发生了变化
+    var cityList = document.getElementById('city-select');
+    var index = cityList.selectedIndex;
+    var cityValue = cityList.options[index].text;
+
+    // 设置对应数据
+    pageState.nowSelectCity = cityValue;
+
+    // 调用图表渲染函数
+    renderChart()
+}
+
+// 初始化日、周、月的radio事件，当点击时，调用函数graTimeChange
+function initGraTimeForm() {
+    graTimeChange();
+    var radioBtn = document.getElementById('form-gra-time');
+    radioBtn.onclick = function (ev) {
+        var ev = ev || window.event;
+        var target = ev.target || ev.srcElement;
+        if (target.nodeName.toLowerCase() == "input") {
+            graTimeChange();
+        }
+    }
+}
+
+// 初始化城市Select下拉选择框中的选项
+function initCitySelector() {
+    // 读取aqiSourceData中的城市，然后设置id为city-select的下拉列表中的选项
     var cityList = document.getElementById('city-select');
     var cityListHtml = '';
     for (var opt in aqiSourceData) {
         cityListHtml += '<option>' + opt + '</option>';
     }
     cityList.innerHTML = cityListHtml;
-}
-
-// 渲染图表
-function renderChart() {
-    var charWrap = document.getElementById('aqi-chart-wrap');
-    var _html = '<h2>' + pageState.nowSelectCity + '空气质量监测数据</h2>';
-
-
-
-}
-
-// 日、周、月的radio事件点击时的处理函数
-function graTimeChange() {
-    // 确定是否选项发生了变化
-
-    // 设置对应数据
-
-    // 调用图表渲染函数
-}
-
-// select发生变化时的处理函数
-function citySelectChange() {
-    // 确定是否选项发生了变化
-
-    // 设置对应数据
-
-    // 调用图表渲染函数
-}
-
-// 初始化日、周、月的radio事件，当点击时，调用函数graTimeChange
-function initGraTimeForm() {
-
-    // var aRadio = document.getElementById();
-
-
-}
-
-// 初始化城市Select下拉选择框中的选项
-function initCitySelector() {
-    // 读取aqiSourceData中的城市，然后设置id为city-select的下拉列表中的选项
 
     // 给select设置事件，当选项发生变化时调用函数citySelectChange
-
+    var optionBtn = document.getElementById('city-select');
+    optionBtn.onclick = function () {
+        citySelectChange();
+    }
 }
 
 // 初始化图表需要的数据格式
@@ -117,7 +152,6 @@ function initAqiChartData() {
 
 // 初始化
 function init() {
-    renderSelect();
     initGraTimeForm()
     initCitySelector();
     initAqiChartData();
